1beb7d8898c89a3fcd5dbb6c38195bbc1a80b00b
MagicRoc <fengyujiancheng@aliyun.com>
2017-03-24 09:59:49 +0800

    lastall

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def outosend():
     """自动发送邮件，递归实现"""
     send()
     global t    #Notice: use global variable!
-    t = threading.Timer(8.0, outosend)    # 定时器
+    t = threading.Timer(86400.0, outosend)    # 定时器
     t.start()
 t = threading.Timer(5.0, outosend)
 while 1:
